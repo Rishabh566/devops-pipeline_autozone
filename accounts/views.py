bedba5127a1bf1cb92f74518e7fc8760c104f27c
Rish <55277468+rishrs7@users.noreply.github.com>
2023-04-13 05:47:36 +0100

    Login/Register form created

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def login(request):
+	return render(request, 'accounts/login.html')
+
+def register(request):
+	if request.method == 'POST' :
+		print('this is POST')
+		return redirect('register')
+	else:
+		return render(request, 'accounts/register.html')
+
+def dashboard(request):
+	return render(request, 'accounts/dashboard.html')
+
+def logout(request):
+	return redirect('home')
